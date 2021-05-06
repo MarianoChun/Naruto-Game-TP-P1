e489e8fca9b7b44ee99b95397dc66e3c66a954c1
AugustKupczyk <augustkup@gmail.com>
2021-05-06 20:53:02 -0300

    add sakura

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,4 +1,5 @@
 package juego;
+import java.awt.Color;
 
 import entorno.Entorno;
 import entorno.InterfaceJuego;
@@ -8,6 +9,8 @@ public class Juego extends InterfaceJuego {
 	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
 	
+	private Sakura sakura;
+	
 	public Juego() {
 		// Inicializa el objeto entorno
 		this.entorno = new Entorno(this, "Prueba del Entorno", 800, 600);
@@ -15,8 +18,10 @@ public class Juego extends InterfaceJuego {
 		// Inicializar lo que haga falta para el juego
 		// ...
 
+		sakura= new Sakura (entorno.ancho()/2,entorno.alto()/2,50,Color.BLUE,1);
 		// Inicia el juego!
 		this.entorno.iniciar();
+		sakura.dibujar(entorno);
 	}
 
 	/**
@@ -28,6 +33,10 @@ public class Juego extends InterfaceJuego {
 	public void tick() {
 		// Procesamiento de un instante de tiempo
 		// ...
+//		sakura.dibujar(entorno);
+	//	sakura.mover();
+	
+		
 		
 	}
 	
