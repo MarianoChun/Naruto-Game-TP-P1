60271dc22a29c4f1098295017d3c0b8632a7acd0
Mariano Chun <marianochun01@gmail.com>
2021-05-07 00:04:44 -0300

    Add Rasengan

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -9,31 +9,52 @@ public class Juego extends InterfaceJuego {
 
 	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
-	private Ninja ninja;
+	private Ninja ninja1;
+	private Rasengan rasengan;
+	boolean vivo = true;
 	public Juego() {
 		// Inicializa el objeto entorno
-		this.entorno = new Entorno(this,  "Sakura Ikebana Delivery - Grupo 4 - v1", 800, 600);
-		this.ninja = new Ninja(100,200,Color.CYAN,2);
+		this.entorno = new Entorno(this, "Sakura Ikebana Delivery - Grupo 4 - v1", 800, 600);
+		this.ninja1 = new Ninja(100, 400, 2, Math.PI / 2, Color.RED);
+		this.rasengan = new Rasengan(100,0,Math.PI/2);
+		
+		
 		// Inicializar lo que haga falta para el juego
 		// ...
 
 		// Inicia el juego!
 		this.entorno.iniciar();
+		// ninja1.dibujar(entorno);
 	}
 
 	/**
-	 * Durante el juego, el método tick() será ejecutado en cada instante y 
-	 * por lo tanto es el método más importante de esta clase. Aquí se debe 
-	 * actualizar el estado interno del juego para simular el paso del tiempo 
-	 * (ver el enunciado del TP para mayor detalle).
+	 * Durante el juego, el método tick() será ejecutado en cada instante y por lo
+	 * tanto es el método más importante de esta clase. Aquí se debe actualizar el
+	 * estado interno del juego para simular el paso del tiempo (ver el enunciado
+	 * del TP para mayor detalle).
 	 */
 	public void tick() {
 		// Procesamiento de un instante de tiempo
 		// ...
-		ninja.dibujar(entorno);
-		ninja.mover();
+		
+		if(vivo) {
+		ninja1.dibujar(entorno);
+		ninja1.mover();
+		}
+		rasengan.dibujar(entorno);
+		rasengan.mover();
+		
+		if (ninja1.chocasteConElEntorno(entorno)) {
+			ninja1.cambiarDeDireccion();
+		}
+		
+		if (ninja1.chocasteConUnRasengan(rasengan)) {
+			//vivo = false;
+			System.out.println("Choca");
+		}
+		
 	}
-	
+
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		Juego juego = new Juego();
---
+++ src/juego/Rasengan.java
@@ -0,0 +1,50 @@
+package juego;
+
+import java.awt.Color;
+import entorno.Entorno;
+
+public class Rasengan {
+	private double x;
+	private double y;
+	private double tamaño;
+	private double angulo;
+	private double velocidad;
+	private Color color;
+	
+	public Rasengan(double x, double y, double angulo) {
+		this.x = x;
+		this.y = y;
+		this.tamaño = 20;
+		this.angulo = angulo;
+		this.velocidad = 3;
+		this.color = Color.BLUE;
+	}
+	
+	public double getTamaño() {
+		return tamaño;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void dibujar(Entorno e) {
+		e.dibujarCirculo(x, y, tamaño, color);
+	}
+	
+	public void mover() {
+		// TODO Auto-generated method stub
+		x += velocidad * Math.cos(angulo);
+		y += velocidad * Math.sin(angulo);
+	}
+	public boolean chocasteConElEntorno(Entorno e) {
+		return x < tamaño/2|| x > e.ancho() - tamaño/2 ||
+				y < tamaño/2 || y > e.alto() -tamaño/2;
+	}
+	
+	
+}
