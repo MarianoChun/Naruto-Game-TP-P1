c579c056a4022d37b79803c7c06b408f4de40784
Mariano Chun <marianochun01@gmail.com>
2021-05-11 19:56:27 -0300

    Add imagenes

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -2,8 +2,10 @@ package juego;
 
 
 import java.awt.Color;
+import java.awt.Image;
 
 import entorno.Entorno;
+import entorno.Herramientas;
 import entorno.InterfaceJuego;
 
 public class Juego extends InterfaceJuego {
@@ -12,38 +14,26 @@ public class Juego extends InterfaceJuego {
 	private Entorno entorno;
 	private Ninja ninja1;
 	private Rasengan rasengan;
-	boolean vivo = true;
 	private Calle calle;
 	private Calle calle1;
 	private Sakura sakura;
-
+	private Image fondo;
 	
-
-//	private Ninja ninja;
-
-	private Ninja ninja;
-
-
 
 	public Juego() {
 		// Inicializa el objeto entorno
 
 		this.entorno = new Entorno(this,  "Sakura Ikebana Delivery - Grupo 4 - v1", 800, 600);
-		this.sakura = new Sakura (400,300,50,5,Color.BLUE);
-		
-				
-
-		
-		this.ninja1 = new Ninja(100, 400, 2, Math.PI / 2, Color.RED);
-		this.rasengan = new Rasengan(100,0,Math.PI/2);
-		
-		
+			
 		// Inicializar lo que haga falta para el juego
 		// ...
+		sakura = new Sakura (400,300,50,5,Color.BLUE);		
+		ninja1 = new Ninja(100, 400, 2);
+		rasengan = new Rasengan(100,0,Math.PI/2);
 		calle = new Calle(400,100, 800,50,0, Color.blue);
 		calle1 = new Calle(400,300, 800,50,0, Color.blue); 
-
-
+		fondo = Herramientas.cargarImagen("fondoJuego.png");
+		
 		
 		// Inicia el juego!
 
@@ -67,10 +57,11 @@ public class Juego extends InterfaceJuego {
 	
 		// Procesamiento de un instante de tiempo
 		// ...
-		calle.dibujar(entorno);
-		calle1.dibujar(entorno);
-		sakura.dibujar(entorno);
-		
+//		calle.dibujar(entorno);
+//		calle1.dibujar(entorno);
+		entorno.dibujarImagen(fondo,400, 300, Math.PI*2);
+		sakura.dibujar(entorno);	
+		ninja1.dibujar(entorno);
 		
 		if(sakura.chocasteConElEntorno(entorno)) {
 			sakura.frenar(entorno);
@@ -93,16 +84,16 @@ public class Juego extends InterfaceJuego {
 		
 		
 		
-		if(vivo) {
-		ninja1.dibujar(entorno);
+		
+		
 		ninja1.mover();
-		}
+		
 		rasengan.dibujar(entorno);
 		rasengan.mover();
 		
-		if (ninja1.chocasteConElEntorno(entorno)) {
-			ninja1.cambiarDeDireccion();
-		}
+//		if (ninja1.chocasteConElEntorno(entorno)) {
+//			ninja1.cambiarDeDireccion();
+//		}
 		
 //		if (ninja1.chocasteConUnRasengan(rasengan)) {
 //			//vivo = false;
